d10ea48ef62a8b980f95719e2f8dd483788f52c4
cuylerjh <cuylerjh@gmail.com>
2024-06-12 11:41:16 -0400

    Update z_player.c
    Change to grabbed state not breaking under water

--- soh/src/overlays/actors/ovl_player_actor/z_player.c
+++ soh/src/overlays/actors/ovl_player_actor/z_player.c
@@ -4082,7 +4082,7 @@ void func_80837C0C(PlayState* play, Player* this, s32 arg2, f32 arg3, f32 arg4, 
 
     if (!func_80837B18(play, this, 0 - this->actor.colChkInfo.damage)) {
         this->stateFlags2 &= ~PLAYER_STATE2_GRABBED_BY_ENEMY;
-        if (!(this->actor.bgCheckFlags & 1) && !(this->stateFlags1 & PLAYER_STATE1_IN_WATER)) {
+        if (!(this->actor.bgCheckFlags & 1)) {
             func_80837B9C(this, play);
         }
         return;
